abd4f50efb5ef29dd942c992c03bb85c81094045
Donatus Brain Okwe <braindona@gmail.com>
2021-12-22 22:05:49 -0600

    Final commit

--- libs/users/src/lib/pages/logn/logn.component.ts
+++ libs/users/src/lib/pages/logn/logn.component.ts
@@ -1,63 +1,63 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
-import { LocalStorageService } from '../../services/local-storage.service';
+// import { HttpErrorResponse } from '@angular/common/http';
+// import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import { AuthService } from '../../services/auth.service';
+// import { LocalStorageService } from '../../services/local-storage.service';
 
-@Component({
-  selector: 'users-logn',
-  templateUrl: './logn.component.html',
-  styles: [
-  ]
-})
-export class LognComponent implements OnInit {
-  loginFormGroup!: FormGroup
-  isSubmitted: boolean = false
-  authError: boolean = false
-  errorMessage: string = 'Invalid Login Details'
+// @Component({
+//   selector: 'users-logn',
+//   templateUrl: './logn.component.html',
+//   styles: [
+//   ]
+// })
+// export class LognComponent implements OnInit {
+//   loginFormGroup!: FormGroup
+//   isSubmitted: boolean = false
+//   authError: boolean = false
+//   errorMessage: string = 'Invalid Login Details'
 
-  constructor(private formBuilder: FormBuilder,
-              private authService: AuthService,
-              private localStorageService: LocalStorageService,
-              private router: Router) { }
+//   constructor(private formBuilder: FormBuilder,
+//               private authService: AuthService,
+//               private localStorageService: LocalStorageService,
+//               private router: Router) { }
 
-  ngOnInit(): void {
-    this.initForm()
-  }
+//   ngOnInit(): void {
+//     this.initForm()
+//   }
 
-  private initForm() {
-    this.loginFormGroup = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    }) 
-  }
+//   private initForm() {
+//     this.loginFormGroup = this.formBuilder.group({
+//       email: ['', [Validators.required, Validators.email]],
+//       password: ['', Validators.required]
+//     }) 
+//   }
 
-  onSubmit() {
-    this.isSubmitted = true
+//   onSubmit() {
+//     this.isSubmitted = true
 
-    if(this.loginFormGroup.invalid) return
+//     if(this.loginFormGroup.invalid) return
 
-    const loginData = {
-      email: this.loginForm.email.value,
-      password: this.loginForm.password.value
-    }
+//     const loginData = {
+//       email: this.loginForm.email.value,
+//       password: this.loginForm.password.value
+//     }
 
-    this.authService.login(loginData).subscribe(data => {
-      this.authError = false
-      this.localStorageService.setToken(data.token)
-      this.router.navigate(['/checkout'])
-      console.log(data)
-    }, (error: HttpErrorResponse) => {
-      this.authError = true
-      if(error.status !== 400) {
-        this.errorMessage = 'Error in the server. Please try again later'
-      }
-    })
-  }
+//     this.authService.login(loginData).subscribe(data => {
+//       this.authError = false
+//       this.localStorageService.setToken(data.token)
+//       this.router.navigate(['/checkout'])
+//       console.log(data)
+//     }, (error: HttpErrorResponse) => {
+//       this.authError = true
+//       if(error.status !== 400) {
+//         this.errorMessage = 'Error in the server. Please try again later'
+//       }
+//     })
+//   }
 
-  get loginForm() {
-    return this.loginFormGroup.controls
-  }
+//   get loginForm() {
+//     return this.loginFormGroup.controls
+//   }
 
-}
+// }
